8701e9883626af7531c9c31c718b046d0b3c3404
Vitor Morais <vitormorais52@gmail.com>
2020-07-15 19:32:54 -0300

    Testes realizados com sucesso.

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -8,8 +8,19 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({title, value, type}: Omit<Transaction, 'id'>): Transaction {
+    
+    //TRATATIVAS / O VALOR DE OUTCOME NAO PODE SER MAIOR QUE O TOTAL DE INCOME
+    if(!["income","outcome"].includes(type)){
+      throw new Error("Transaction type is invalid");
+    }
+
+    const {total} = this.transactionsRepository.getBalance();
+    if(type == "outcome" && total < value){
+      throw new Error("You do not have enough balance");
+    }
+    const transaction: Transaction = this.transactionsRepository.create({title, value, type});
+    return transaction;
   }
 }
 
